b8a39685996956ad7ddbf0898afe986ed861fefd
gopal vaghasiya <gopalpv.sbccas1314@gmail.com>
2017-10-30 19:15:39 +0530

    json learn

---
+++ json.js
@@ -0,0 +1,20 @@
+// i will make a changes because of git learning
+
+//pipe
+var http=require('http');
+var fs=require('fs');
+
+var server=http.createServer(function(req,res){
+    console.log('request URL '+req.url);
+    res.writeHead(200,{'Content-Type':'application/json'});
+    var myObj={
+        name: 'gopal',
+        job:'tp',
+        age:29
+    };
+    res.end(JSON.stringify(myObj));
+    
+});
+
+server.listen(3000,'127.0.0.1');
+console.log('lisning 3000 port');
